f74f6654aff0869ff2ae9106e98f2628bf700a03
Heine Furubotten <hfurubotten@users.noreply.github.com>
2020-03-18 23:59:07 +0100

    Move dto objects to own package
    Added test cases for data parsing

---
+++ enturclient/dto/__init__.py
@@ -0,0 +1,5 @@
+"""Data transfer object for Entur's API objects."""
+from .estimated_call import EstimatedCall
+from .place import Place
+
+__all__ = ["EstimatedCall", "Place"]
--- enturclient/dto/estimated_call.py
+++ enturclient/dto/estimated_call.py
@@ -1,53 +1,5 @@
+"""Data transfer object for estimated call object."""
 from datetime import datetime, timedelta
-
-
-class Place:
-    """Represents a stop place or platform from entur."""
-
-    def __init__(self, data: dict, is_platform: bool):
-        """Initialize the place object."""
-        self._data = data
-        self.is_platform = is_platform
-
-    @property
-    def place_id(self) -> str:
-        """Id for the stop place or platform."""
-        return self._data["id"]
-
-    @property
-    def name(self) -> str:
-        """Friendly name for the stop place or platform"""
-        if self.is_platform:
-            if self._data["publicCode"]:
-                return self._data["name"] + " Platform " + self._data["publicCode"]
-            else:
-                return self._data["name"] + " Platform " + self.place_id.split(":")[-1]
-        else:
-            return self._data["name"]
-
-    @property
-    def latitude(self) -> float:
-        """Latitude part of place location."""
-        return self._data.get("latitude")
-
-    @property
-    def longitude(self) -> float:
-        """Longitude part of place location."""
-        return self._data.get("longitude")
-
-    @property
-    def public_code(self) -> int:
-        return self._data.get("publicCode")
-
-    @property
-    def estimated_calls(self):
-        """List estimated calls from the place."""
-        return [EstimatedCall(s) for s in self._data["estimatedCalls"]]
-
-    @property
-    def raw(self):
-        """Raw data for the place from the API."""
-        return self._data
 
 
 class EstimatedCall:
@@ -95,12 +47,12 @@ class EstimatedCall:
 
     @property
     def delay_in_min(self) -> int:
-        """Calculated delay in minutes."""
+        """Calculate delay in minutes."""
         return int(self.delay.total_seconds() / 60)
 
     @property
     def delay(self) -> timedelta:
-        """Calculated delay."""
+        """Calculate delay."""
         return self.expected_departure_time - self.aimed_departure_time
 
     @property
